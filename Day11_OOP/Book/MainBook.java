116c7f7d8593cbe632fa7db4cda8c7579114c603
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-09 09:28:14 +0200

    .

--- Day11_OOP/Book/MainBook.java
+++ Day11_OOP/Book/MainBook.java
@@ -4,28 +4,27 @@ public class MainBook {
     public static void main(String[] args) {
         ArrayList<Book> books = new ArrayList<>();
         Book book1 = new Book("Harper Lee", "9780061120084", 324, "To Kill a Mockingbird");
-        book1.printDetails();
         books.add(book1);
         Book book2 = new Book("George Orwell", "9780451524935", 328, "1984");
-        book2.printDetails();
         books.add(book2);
         Book book3 = new Book("Jane Austen", "9781503290563", 279, "Pride and Prejudice");
-        book3.printDetails();
         books.add(book3);
         Book book4 = new Book("F. Scott Fitzgerald", "9780743273565", 180, "The Great Gatsby");
-        book4.printDetails();
         books.add(book4);
         Book book5 = new Book("Herman Melville", "9781503280786", 635, "Moby Dick");
-        book5.printDetails();
         books.add(book5);
+
+        for (Book book : books) {
+            book.printDetails();
+        }
 
         String isbnToSearch = "9780743273565";
         Book bookFound = Book.sucheNachISBN(books, isbnToSearch);
 
         if (bookFound != null) {
-            System.out.println("Following book was found");
+            System.out.printf("Following book with isbn(%s) was found%n", isbnToSearch);
             bookFound.printDetails();
-        } else System.out.println("Book not found, try with a another isbn!");
+        } else System.out.printf("Book not with isbn(%s) found, try with a another isbn!", isbnToSearch);
     
     }
 }
---
+++ Day11_OOP/Kunden/MainKunde.java
@@ -0,0 +1,13 @@
+
+public class MainKunde {
+    public static void main(String[] args) {
+
+        Kunde kunde1 = new Kunde("ciao@gmail.com", "DE 123456", "Vittorio De Marzi", "0049 12345675");
+        
+        kunde1.printDetails();
+
+    }
+    
+    
+
+}
