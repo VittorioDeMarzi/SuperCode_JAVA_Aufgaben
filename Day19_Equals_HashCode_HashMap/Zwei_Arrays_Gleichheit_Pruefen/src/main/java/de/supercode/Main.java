e9e7b0b466c51a47751f94d2ae907bd425613e1d
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-26 09:38:22 +0200

    modify Zwei Arrays auf Gleichheit prüfen

--- Day19_Equals_HashCode_HashMap/Zwei_Arrays_Gleichheit_Pruefen/src/main/java/de/supercode/Main.java
+++ Day19_Equals_HashCode_HashMap/Zwei_Arrays_Gleichheit_Pruefen/src/main/java/de/supercode/Main.java
@@ -7,21 +7,26 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] list1 = {1,1,1,1,1,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6};
-        int[] list2 = {1,1,1,1,1,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,6};
+        int[] list1 = {1,1,1,1,1,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,9};
+        int[] list2 = {1,1,1,1,1,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,6,5,9};
 
-        System.out.println(haeufigkeitPruefen(list1, list2));
+        haeufigkeitPruefen(list1, list2);
     }
 
-    private static boolean haeufigkeitPruefen(int[] list1, int[] list2) {
+    private static void haeufigkeitPruefen(int[] list1, int[] list2) {
         HashMap<Integer, Integer> list1Map = getCount(list1);
         HashMap<Integer, Integer> list2Map = getCount(list2);
         boolean result = true;
+        if (list1Map.size() != list2Map.size()) {
+            System.out.println("Arrays don't have the same frequency of numbers");
+            return;
+        }
         for (Integer key : list1Map.keySet())
             if (list1Map.get(key) != list2Map.get(key)) {
-                return false;
+                System.out.println("Arrays don't have the same frequency of numbers");
+                return;
             }
-        return true;
+        System.out.println("Arrays have the same frequency of numbers");
     }
 
     private static HashMap<Integer, Integer> getCount(int[] list) {
